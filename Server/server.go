6e245420cf063507ec99df3ea90afbfa320643ad
johnmops <evgenibir@outlook.com>
2020-12-18 13:41:11 +0200

    developing the server.go

--- Server/server.go
+++ Server/server.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	//"os"
+	//"github.com/joho/godotenv"
 )
+
+var tpl = template.Must(template.ParseFiles("./static/index.html"))
+var dogTpl = template.Must(template.ParseFiles("./static/indexDog.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request)  {
+	tpl.Execute(w, nil)
+}
 
 func helloHanlder(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
@@ -21,8 +31,12 @@ func helloHanlder(w http.ResponseWriter, r *http.Request) {
 }
 
 func dog(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Dog Page")
-	return
+	dogTpl.Execute(w, nil)
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not allowed", http.StatusForbidden)
+		return
+	}
 }
 
 func main() {
@@ -30,6 +44,7 @@ func main() {
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHanlder)
 	http.HandleFunc("/dog", dog)
+	http.HandleFunc("/index", indexHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
